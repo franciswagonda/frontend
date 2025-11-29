d1505a455328c9fa85834c9b7dc025f08c401857
fridahnankwanga <fridahnankwanga403@gmail.com>
2025-11-29 08:06:17 +0300

    Add files via upload

---
+++ Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Login = () => {
+    return (
+        <div style={{ padding: '2rem', maxWidth:'400px',
+            margin: '0 auto'}}>
+            <h1>Login</h1>
+            <p>Login form wil go here</p>
+            </div>
+    );
+};
+export default Login;
