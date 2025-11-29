c62b4c6674790dd91325674f014885e398801e52
fridahnankwanga <fridahnankwanga403@gmail.com>
2025-11-29 08:05:42 +0300

    Add files via upload

---
+++ Gallery.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Gallery = () => {
+    return (
+        <div style={{ padding: '2rem', maxWidth:'1200px',
+            margin: '0 auto'}}>
+            <h1>Gallery</h1>
+            <p>Browse all approved projects here.</p>
+            </div>
+    );
+};
+export default Gallery;
